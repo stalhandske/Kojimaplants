1f0b33e0fbd145246a02060c086c51e18e5c7de9
Simon Stålhandske <simon.stalhandske@gmail.com>
2018-08-04 14:08:27 +0200

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public GameObject seedPrefab;
+    public float throwSpeed;
+    public float throwRotation;
+
+    Rigidbody2D _rigidbody2D;
+
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
+	void FixedUpdate ()
+	{
+        Vector2 axisInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+	    float xVelocity = speed * axisInput.x * Time.deltaTime;
+
+        _rigidbody2D.velocity = new Vector2(xVelocity, _rigidbody2D.velocity.y);
+	}
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Seed seed = Instantiate(seedPrefab, transform.position + Vector3.up*1.5f, Quaternion.identity)
+                .GetComponent<Seed>();
+            seed.GetComponent<Rigidbody2D>().velocity = (Random.insideUnitCircle + Vector2.up*2)* throwSpeed;
+            seed.GetComponent<Rigidbody2D>().angularVelocity = Random.Range(-throwRotation, throwRotation);
+        }
+    }
+}
---
+++ Assets/Scripts/Seed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seed : MonoBehaviour
+{
+    public GameObject plantPrefab;
+    public float checkForPlantRadius;
+    public LayerMask plantLayer;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Collider2D hit = Physics2D.OverlapCircle(col.contacts[0].point, checkForPlantRadius, plantLayer);
+        if (col.collider.CompareTag("Ground") && col.contacts[0].normal.y > .5f && !hit)
+        {
+            Vector2 seedHitPosition = col.contacts[0].point;
+            seedHitPosition.y = Mathf.Round(seedHitPosition.y);
+            Instantiate(plantPrefab, seedHitPosition, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}

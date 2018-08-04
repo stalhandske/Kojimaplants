8b78ec0c8187691f159ffa62cd2574b77690153f
Simon Stålhandske <simon.stalhandske@gmail.com>
2018-08-04 18:06:57 +0200

    made a bird

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -8,13 +8,28 @@ public class Bird : MonoBehaviour
     public float repulseForce;
     public LayerMask repulseLayer;
     public float repulseDistance;
+    public float attractionToStartPos;
 
     Vector2 _checkDirection = Vector2.up;
+    Rigidbody2D _rigidbody2D;
+    Vector2 _startPosition;
 
-	void Update ()
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _startPosition = transform.position;
+    }
+
+	void FixedUpdate ()
 	{
-	    _checkDirection = Quaternion.AngleAxis(.45f, Vector3.forward)*_checkDirection;
+	    _checkDirection = Quaternion.AngleAxis(30, Vector3.forward)*_checkDirection;
 
+	    RaycastHit2D hit = Physics2D.Raycast(transform.position, _checkDirection, repulseDistance, repulseLayer);
+	    if (hit)
+	    {
+            _rigidbody2D.AddForce(repulseForce*-_checkDirection*Time.deltaTime);
+	    }
 
+        _rigidbody2D.AddForce(attractionToStartPos*((Vector3)_startPosition - transform.position).normalized*Time.deltaTime);
 	}
 }
